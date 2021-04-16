7a6cb93373fff4e2a357700cee7a5a179e7c29f4
choiJunHo <wnsghqwe@gmail.com>
2021-04-16 09:26:59 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_app_middle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_app_middle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/myanimatedContainer.dart
@@ -0,0 +1,66 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyAnimatedContainer(),
+    );
+  }
+}
+
+class MyAnimatedContainer extends StatefulWidget {
+  @override
+  _AnimatedContainerState createState() => _AnimatedContainerState();
+}
+
+class _AnimatedContainerState extends State<MyAnimatedContainer> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadius _borderRadius = BorderRadius.circular(8);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AnimatedContainer'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            borderRadius: _borderRadius,
+            color: _color,
+          ),
+          duration: Duration(seconds: 1),
+          curve: Curves.fastOutSlowIn,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            final random = Random();
+            _width = random.nextInt(300).toDouble();
+            _height = random.nextInt(300).toDouble();
+            _color = Color.fromRGBO(random.nextInt(256), random.nextInt(256),
+                random.nextInt(256), 1);
+            _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
+          });
+        },
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
