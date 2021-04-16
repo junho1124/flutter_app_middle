d7eab638a0b041583da953c238e29ee4a55459fb
choiJunHo <wnsghqwe@gmail.com>
2021-04-16 09:49:23 +0900

    투명도 조절 위젯 animatedOpacity.dart

--- lib/animatedOpacity.dart
+++ lib/animatedOpacity.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -25,15 +24,33 @@ class MyAnimatedOpacity extends StatefulWidget {
 }
 
 class _MyAnimatedOpacityState extends State<MyAnimatedOpacity> {
+  bool _visible = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('AnimatedOpacity'),
       ),
-      body: Container(),
-      floatingActionButton:
-          FloatingActionButton(child: Icon(Icons.play_arrow),onPressed: null,),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _visible ? 1.0 : 0.0,
+          duration: Duration(seconds: 1),
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.green,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.play_arrow),
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+      ),
     );
   }
 }
