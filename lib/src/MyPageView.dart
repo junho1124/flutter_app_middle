c9868f0cbbf85aeed79867b7b2e3496ecf1b786a
choiJunHo <wnsghqwe@gmail.com>
2021-04-19 22:04:44 +0900

    TabBar 사용하기

---
+++ lib/src/MyPageView.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyPageView extends StatefulWidget {
+  @override
+  _MyPageViewState createState() => _MyPageViewState();
+}
+
+class _MyPageViewState extends State<MyPageView> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Text('자동차'),),
+                Tab(icon: Icon(Icons.directions_transit),),
+                Tab(icon: Icon(Icons.directions_bike),),
+              ],
+            ),
+            title: Text('Tabs Demo'),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
