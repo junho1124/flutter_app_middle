ad1cf508c18911222c79b119f43e41e5aa472286
choiJunHo <wnsghqwe@gmail.com>
2021-04-28 10:30:05 +0900

    detail update

--- lib/movie/detailPage.dart
+++ lib/movie/detailPage.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_middle/movie/results.dart';
 
 class MovieDetailPage extends StatelessWidget {
   final List<Results> movies;
   final int i;
+
   MovieDetailPage(this.movies, this.i);
 
   @override
@@ -11,69 +13,120 @@ class MovieDetailPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text(movies[i].title,
-        style: TextStyle(
-        ),),
+        title: Text(
+          movies[i].title,
+          style: TextStyle(),
+        ),
       ),
-      body: ListView(shrinkWrap: true,
-        padding: EdgeInsets.all(15.0),
-        children: [
-
+      body:
+          ListView(shrinkWrap: true, padding: EdgeInsets.all(15.0), children: [
         Column(
           children: [
-            Text(movies[i].title,
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-            ),),
-            Text('\'${movies[i].originalTitle}\'',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.blueGrey
-            ),),
-            Image.network('https://image.tmdb.org/t/p/w500/${movies[i].posterPath}',height: 500,),
+            Text(
+              movies[i].title,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '\'${movies[i].originalTitle}\'',
+              style: TextStyle(fontSize: 20, color: Colors.blueGrey),
+            ),
+            Image.network(
+              'https://image.tmdb.org/t/p/w500/${movies[i].posterPath}',
+              height: 500,
+            ),
             SizedBox(
               height: 16,
             ),
             Container(
               alignment: Alignment.topCenter,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.yellow
-              ),
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.yellow),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text('줄거리',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold
-                ),),
+                child: Text(
+                  '줄거리',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
               ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '개봉일: ${movies[i].releaseDate}',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.amberAccent,
+                        ),
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              movies[i].voteCount.toString(),
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
+                            Icon(
+                              Icons.check,
+                              size: 15,
+                            )
+                          ],
+                        )),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.amberAccent,
+                      ),
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Text(movies[i].voteAverage.toString(),
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.bold)),
+                          Icon(
+                            Icons.star_border,
+                            size: 15,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Container(
                 decoration: BoxDecoration(
                   color: Color.fromRGBO(219, 219, 219, 1),
-                  border: Border.all(color: Colors.blueGrey, width: 1),
+                  // border: Border.all(color: Colors.blueGrey, width: 1),
                   borderRadius: BorderRadius.circular(8.0),
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Text(movies[i].overview,
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
-                  ),
+                  child: Text(
+                    movies[i].overview,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
             )
           ],
         ),
-        ]
-      ),
+      ]),
     );
   }
 }
