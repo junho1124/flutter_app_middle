cf46a2217ab53af6d0ea9f87dc29cccf101df9a0
choiJunHo <wnsghqwe@gmail.com>
2021-05-04 15:29:49 +0900

    https로 수정

--- lib/movie/movie_provider.dart
+++ lib/movie/movie_provider.dart
@@ -12,7 +12,7 @@ class MovieProvider extends ChangeNotifier {
 
   Future<MovieResult> fetchData() async {
     var uri = Uri.parse(
-        'http://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1');
+        'https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1');
     var response = await http.get(uri);
 
     MovieResult result = MovieResult.fromJson(json.decode(response.body));
